7108ee7efb44a0e50bf3b0ce4ebb088426a25562
JMRante <joshuamstark@gmail.com>
2015-03-02 21:16:03 -0700

    Second? Added a new class, Verb.

---
+++ LudoNarrareEditPrototype/Verb.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LudoNarrareEditPrototype
+{
+    class Verb
+    {
+        /* Variables */
+        public string name;
+
+        /* Functions */
+        public Verb(string _name)
+        {
+            name = _name;
+        }
+
+        public void setName(string _name)
+        {
+            name = _name;
+        }
+
+        public Verb copy(Verb verb)
+        {
+            verb.setName(name);
+            return verb;
+        }
+
+        public override string ToString()
+        {
+            return name;
+        }
+    }
+}
